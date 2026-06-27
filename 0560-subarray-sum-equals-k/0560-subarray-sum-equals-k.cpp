6067c18225b32e2feeb388d97f589e2d76051345
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-06-27 23:13:36 +0530

    Time: 31 ms (95.88%), Space: 45.3 MB (71.24%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int ans = 0,currSum=0;
+        for(int i=0;i<nums.size();i++){
+            currSum+=nums[i];
+            int complement = -1*(k-currSum);
+            if(mp.find(complement)!=mp.end()){
+                ans+=mp[complement];
+            }
+            mp[currSum]+=1;
+        }
+        return ans;
+    }
+};
