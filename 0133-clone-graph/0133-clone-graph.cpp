6d435ce05840d6e8b5c73b9a134e95d969117883
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-06-29 02:29:34 +0530

    Time: 7 ms (22.76%), Space: 11.9 MB (80.31%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return NULL;
+        queue<Node*> q;
+        unordered_set<int> visited;
+        unordered_map<int,Node*> mp;
+        Node* copy = new Node(node->val);
+        mp[node->val] = copy;
+        q.push(node);
+        visited.insert(node->val);
+        while(q.size()){
+            Node* parent = q.front();
+            q.pop();
+            // iterate nbrs of parent and assign to clone
+            for(auto nbr:parent->neighbors){
+                if(mp.find(nbr->val)==mp.end()){
+                    mp[nbr->val] = new Node(nbr->val);
+                }
+                if(!visited.count(nbr->val)){
+                    visited.insert(nbr->val);
+                    q.push(nbr);
+                }
+                Node* parentCopy = mp[parent->val];
+                Node* childCopy = mp[nbr->val];
+                parentCopy->neighbors.push_back(childCopy);
+            }
+        }
+        return copy;
+    }
+};
