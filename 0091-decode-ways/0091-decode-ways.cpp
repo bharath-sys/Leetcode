b8d4ea3088a4a2a6a8ccbf8373e6b6d9bd21d4f9
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-07-17 22:26:45 +0530

    Time: 0 ms (100%), Space: 9.8 MB (17.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(string s,int idx){
-        if(idx==s.length())return 1;
-        if(s[idx]=='0')return 0;
-        if(dp[idx])return dp[idx];
-        // one char 
-        int ways = solve(s,idx+1);
-        // two char
-        if(idx+1<s.length()){
-            string substr = s.substr(idx,2);
-            if(substr>="10" && substr<="26")
-                ways+=solve(s,idx+2);
+    int solve(string& s, int idx, vector<int>& dp) {
+        if (idx >= s.length())
+            return 1;
+        if (dp[idx] != -1)
+            return dp[idx];
+        int ways = 0;
+        for (int len = 1; len <= 2; len++) {
+            if (idx + len <= s.length()) {
+                string substring = s.substr(idx, len);
+                if (substring[0] == '0')
+                    break;
+                int val = stoi(substring);
+                if (val >= 1 && val <= 26) {
+                    ways += solve(s, idx + len, dp);
+                }
+            }
         }
-        return dp[idx]=ways;
+        return dp[idx] = ways;
     }
+
     int numDecodings(string s) {
-        if(s[0]=='0')return 0;
-        dp.resize(s.length(),0);
-        return solve(s,0);
+        if (s.empty())
+            return 0;
+        vector<int> dp(s.length(), -1);
+        return solve(s, 0, dp);
     }
 };
