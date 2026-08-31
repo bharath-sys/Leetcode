090179316eeb9376829e04cd12107fdeb840ba83
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-08-31 18:51:47 +0530

    Time: 0 ms (100%), Space: 9.7 MB (18.25%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(string s, int idx) {
-        if (idx == s.length())
+    vector<int> memo;
+    int solve(string& s, int idx, string currNum) {
+        if (idx >= s.length())
             return 1;
-        if (s[idx] == '0')
+        else if (memo[idx] != -1)
+            return memo[idx];
+        if(s[idx]=='0'){
             return 0;
-        if (dp[idx] != -1)
-            return dp[idx];
-        // one char
-        int ways = solve(s, idx + 1);
-        // two char
-        if (idx + 1 < s.length()) {
-            string substr = s.substr(idx, 2);
-            if (substr >= "10" && substr <= "26")
-                ways += solve(s, idx + 2);
         }
-        return dp[idx] = ways;
+        int singleDigit =  solve(s, idx + 1, s.substr(idx,1));
+        int doubleDigit = 0;
+        if(idx<s.length()-1 && ((s[idx]=='1' && s[idx+1]<='9') || (s[idx]=='2' && s[idx+1]<='6'))) {
+            doubleDigit = solve(s, idx + 2, s.substr(idx, 2));
+        }
+        return memo[idx] = singleDigit + doubleDigit;
     }
     int numDecodings(string s) {
-        int n = s.length();
-        dp.resize(n + 1, 0);
-        dp[n] = 1;
-        dp[n - 1] = s[n - 1] == '0' ? 0 : 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (s[i] == '0') {
-                dp[i] = 0;
-                // continue;
-            } else {
-                // if single charected ...
-                dp[i] = dp[i + 1];
-                // considering two charecters ...
-                if (i + 1 < n &&
-                    (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
-                    dp[i] += dp[i + 2];
-                }
-            }
-        }
-        return dp[0];
+        memo.resize(s.length(), -1);
+        return solve(s, 0, "");
     }
 };
