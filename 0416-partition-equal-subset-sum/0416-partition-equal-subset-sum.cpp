78a0063543093d97ca1357d57275b05f32aef7df
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-01 01:10:22 +0530

    Time: 47 ms (94.2%), Space: 110.6 MB (34.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool isSumPossible(int target,vector<int> &nums,int idx){
-        if(target==0) return true;
-        if(idx>=nums.size())return false;
-        if(dp[target][idx]!=-1)return bool(dp[target][idx]);
-        if(target>=nums[idx]){
-            return dp[target][idx] = (isSumPossible(target-nums[idx],nums,idx+1) || isSumPossible(target,nums,idx+1));
-        }
-        else return dp[target][idx] = false;
+    vector<vector<int>> memo;
+    bool solve(vector<int>& nums, int idx, int remaining) {
+        if (remaining == 0)
+            return true;
+        else if (idx >= nums.size())
+            return false;
+        if (memo[idx][remaining] != -1)
+            return bool(memo[idx][remaining]);
+        bool take = false;
+        if (nums[idx] <= remaining) {
+            return memo[idx][remaining] =
+                       solve(nums, idx + 1, remaining - nums[idx]) ||
+                       solve(nums, idx + 1, remaining);
+        } else
+            return memo[idx][remaining] = false;
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0)return false;
-        sort(nums.begin(),nums.end());
-        dp.resize((sum/2)+1,vector<int>(nums.size(),-1));
-        return isSumPossible(sum/2,nums,0);
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % 2)
+            return false;
+        sort(nums.begin(), nums.end());
+        // check if even sum of total/2 is possible
+        memo.resize(nums.size(), vector<int>((total / 2) + 1, -1));
+        return solve(nums, 0, total / 2);
     }
 };
