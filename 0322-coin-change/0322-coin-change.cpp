3e47d16748408321aefa45ea97bc97c7f83ec1fb
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-03 14:10:39 +0530

    Time: 55 ms (19.96%), Space: 52.9 MB (14.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> memo;
-    int solve(vector<int>& coins, int target, int idx) {
-        if (target == 0) {
-            return 0;
-        }
-        if (idx >= coins.size())
-            return 1e9;
-        if (memo[idx][target] != -1)
-            return memo[idx][target];
-        int notTakeVal = solve(coins, target, idx + 1);
-        int takeVal = 1e9;
-        if (target >= coins[idx]) {
-            takeVal = 1+solve(coins, target - coins[idx], idx);
-        }
-        return memo[idx][target] = min(notTakeVal,takeVal);
-    }
+
     int coinChange(vector<int>& coins, int amount) {
         if (amount == 0)
             return 0;
-        memo.resize(coins.size(), vector<int>(amount + 1, -1));
-        int ans = solve(coins, amount, 0);
-        return ans == 1e9 ? -1 : ans;
+        int n = coins.size();
+        const int INF = 1e9;
+        memo.resize(n + 1, vector<int>(amount + 1, INF));
+        // target = 0 requires 0 coins
+        for (int i = 0; i <= n; i++) {
+            memo[i][0] = 0;
+        }
+        // Tabulation
+        for (int i = n - 1; i >= 0; i--) {
+            for (int target = 1; target <= amount; target++) {
+                // Take
+                int take = INF;
+                if (target >= coins[i]) {
+                    take = 1 + memo[i][target - coins[i]];
+                }
+                // Not take
+                int notTake = memo[i + 1][target];
+                memo[i][target] = min(take, notTake);
+            }
+        }
+        return memo[0][amount] == INF ? -1 : memo[0][amount];
     }
 };
