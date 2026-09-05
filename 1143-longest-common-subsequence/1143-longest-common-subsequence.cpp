12663d3982418a77512761eb9bbf29f2a6992ebd
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-06 01:55:14 +0530

    Time: 26 ms (66.58%), Space: 27.5 MB (55.88%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -8,13 +8,22 @@ public:
         if(s1[idx1]==s2[idx2]){
             len = 1+solve(s1,s2,idx1+1,idx2+1);
         }
-        return memo[idx1][idx2] = max(memo[idx1][idx2],len);
+        return memo[idx1][idx2] = len;
     }
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length();
         int n = text2.length();
         // memo idx,position
-        memo.resize(m,vector<int>(n,-1));
-        return solve(text1,text2,0,0);
+        memo.resize(m+1,vector<int>(n+1,0));
+        for(int idx1=m-1;idx1>=0;idx1--){
+            for(int idx2=n-1;idx2>=0;idx2--){
+                memo[idx1][idx2] = max(memo[idx1+1][idx2],memo[idx1][idx2+1]);
+                if(text1[idx1]==text2[idx2]){
+                    memo[idx1][idx2] = 1+memo[idx1+1][idx2+1];
+                }
+            }
+        }
+        return memo[0][0];
+        // return solve(text1,text2,0,0);
     }
 };
