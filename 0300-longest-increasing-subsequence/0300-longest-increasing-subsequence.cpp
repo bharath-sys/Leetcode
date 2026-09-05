b8933529efec140c4bcb2e570608594496fc2948
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-06 00:26:37 +0530

    Time: 0 ms (100%), Space: 13.9 MB (94.74%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
+    int custom_lower_bound(vector<int> &nums,int ele){
+        int l,r;
+        l = 0; r = nums.size()-1;
+        int lb = nums.size();
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m]<ele){
+                l = m+1;
+            }
+            else {
+                lb = m;
+                r = m-1;
+            }
+        }
+        return lb;
+    }
     int lengthOfLIS(vector<int>& nums) {
         if (nums.empty()) return 0;
         vector<int> lis;
         lis.push_back(nums[0]);
         for(int i=1;i<nums.size();i++){
             // find lowerbound
-            int lb = lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
+            // int lb = lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
+            int lb = custom_lower_bound(lis,nums[i]);
             if(lb==lis.size()){
                 lis.push_back(nums[i]);
             }
