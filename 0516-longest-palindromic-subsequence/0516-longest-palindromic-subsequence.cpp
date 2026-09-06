0b3d1d042cd3c56309854098f553319b3063ac09
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-07 01:28:35 +0530

    Time: 50 ms (79.71%), Space: 75.9 MB (65.08%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> memo;
-    int solve(string &s,int i,int j){
-        if(i>j)return 0;
-        if(i==j)return 1;
-        if(memo[i][j]!=-1)return memo[i][j];
-        // will write memo later
-        // 1.  if both are equal
-        if(s[i]==s[j]){
-            memo[i][j] = 2+solve(s,i+1,j-1);
-        }
-        else memo[i][j] = max(solve(s,i+1,j),solve(s,i,j-1));
-        return memo[i][j];
-    }
     int longestPalindromeSubseq(string s) {
-        // memo.resize(s.length(),vector<int> (s.length(),-1));
         memo.resize(s.length(),vector<int> (s.length(),0));
-        for(int i=0;i<=s.length()-1;i++){
-            for(int j=0;j<=s.length()-1;j++){
-                if(j==i)memo[i][j] = 1;
-            }
-        }
         for(int i=s.length()-1;i>=0;i--){
+            memo[i][i] = 1;
             for(int j=i+1;j<s.length();j++){
                 if(s[i]==s[j]){
                     memo[i][j] = 2+memo[i+1][j-1];
