6476bb0ba7fa76358ca53a4f5a14302c1e032503
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-11 14:19:24 +0530

    Time: 43 ms (82.67%), Space: 29 MB (73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,20 +4,19 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for(int i=0;i<nums.size();i++){
-            while(i>0 && i<nums.size() && nums[i]==nums[i-1])i++;
+            if(i>0 && nums[i]==nums[i-1])continue;
             int j=i+1;int k=nums.size()-1;
             while(j<k){
                 int sum = nums[i]+nums[j]+nums[k];
                 if(sum==0){
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j+=1;k-=1;
+                    while(j>i+1 && j<k && nums[j]==nums[j-1])j++;
                 }
                 else if(sum>0){
                     k-=1;
                 }
                 else j+=1;
-                while(j>i+1 && j<k && nums[j]==nums[j-1])j++;
-                while(k<nums.size()-1 && k>j && nums[k]==nums[k+1])k--;
             }
         }
         return ans;
