27a3d594d8f49f9666481764c780402b4904aa84
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-21 00:18:37 +0530

    Time: 33 ms (39.11%), Space: 56 MB (29.07%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-     int numSubseq(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        int ans = 0;int l = 0;int r = n-1;int mod = 1e9+7;
-        int* pows = new int[n];
-        pows[0] = 1;
-        for(int i=1;i<n;i++){
-            pows[i] = (pows[i-1]*2)%mod;
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int l = 0;
+        int r = nums.size() - 1;
+        long long ans = 0;
+        int mod = 1e9 + 7;
+        vector<long long> power(nums.size() + 1, 1);
+
+        for (int i = 1; i <= nums.size(); i++) {
+            power[i] = (power[i - 1] * 2) % mod;
         }
-        while(l<=r){
-            if(nums[l]+nums[r]>target){
-                r-=1;
-            }
-            else{
-                ans = (ans + pows[(2,(r-l++))])%mod;
-            }
+
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                ans+=power[r-l];
+                ans%=mod;
+                l++;
+            } else
+                r -= 1;
         }
-        return ans;
+        return ans % mod;
     }
 };
