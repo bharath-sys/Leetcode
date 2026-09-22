a30c40fdea4bbf81864d58f7f7b727e62bb7e0c3
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-23 00:01:46 +0530

    Time: 0 ms (100%), Space: 14.5 MB (42.83%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -3,10 +3,12 @@ public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         vector<int> nums(1001,0);
         for(int i=0;i<trips.size();i++){
-            for(int j=trips[i][1];j<trips[i][2];j++){
-                if(nums[j]+trips[i][0]>capacity)return false;
-                nums[j]+=trips[i][0];
-            }
+            nums[trips[i][1]]+=trips[i][0];nums[trips[i][2]]-=trips[i][0];
+        }
+        int load = 0;
+        for(auto x:nums){
+            load+=x;
+            if(load>capacity)return false;
         }
         return true;
     }
