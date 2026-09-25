acfcee47ecc13bc9108b5100b0883a36b71b290d
bharath-sys <83537750+bharath-sys@users.noreply.github.com>
2026-09-25 23:41:54 +0530

    Time: 59 ms (81.05%), Space: 44 MB (25.98%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,17 +10,20 @@
 class Solution {
 public:
     TreeNode* ans = nullptr;
-    TreeNode* solve(TreeNode* root,TreeNode *p,TreeNode *q){
-        if(!root) return nullptr;
-        auto l = solve(root->left,p,q);
-        auto r = solve(root->right,p,q);
-        if((l==p || r==p || root==p) && (l==q || r==q || root==q) ){
+    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root)
+            return nullptr;
+        auto l = solve(root->left, p, q);
+        if ((p == root || p == l) && (l == q || root == q))
             return root;
-        }
-        if(p==root || q==root)return root;
+        auto r = solve(root->right, p, q);
+        if ((l == p || r == p || root == p) && (l == q || r == q || root == q))
+            return root;
+        if (p == root || q == root)
+            return root;
         return l ? l : r;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-       return solve(root,p,q);
+        return solve(root, p, q);
     }
 };
